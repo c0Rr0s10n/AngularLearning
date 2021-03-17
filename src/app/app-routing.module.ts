5d8c980fc8d1a5f02ec78f13cd8c0c066203d6cf
Alex <youdoit@mail.ru>
2021-03-18 01:29:59 +0600

    Trying to repeat a new example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { ChildAComponent } from './child-a/child-a.component';
-import { ChildBComponent } from './child-b/child-b.component';
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent,children: [
-    {
-      path: 'child-a',
-      component: ChildAComponent,
-    },
-    {
-      path: 'child-b',
-      component: ChildBComponent,
-    }
-  ]},
-  {
-    path: 'second-component',
-    component: SecondComponent,
-
-  },
-  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { ChildAComponent } from './child-a/child-a.component';
-import { ChildBComponent } from './child-b/child-b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent,
-    SecondComponent,
-    ChildAComponent,
-    ChildBComponent
   ],
   imports: [
     BrowserModule,
