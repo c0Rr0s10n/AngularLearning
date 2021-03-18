1c82caeac57c6cd39fcdb8057f51a7b581330e57
Alex <youdoit@mail.ru>
2021-03-19 03:19:28 +0600

    Too tired to continue learning for today

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'AngularLearning';
